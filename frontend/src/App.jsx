eee8d47db73ba33c42f7beecec9dc3dc511645e4
Joyanna Jiang <jfjiang@usc.edu>
2025-05-22 17:01:11 -0700

    inital frontend commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import './App.css'
+import Avatar from '@mui/material/Avatar';
+import Container from '@mui/material/Container';
+
+
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Container maxWidth="lg">
+    
+          I just farted and it was really smelly
+          <Avatar>JJ</Avatar>
+      
+      </Container>
+     
+    </>
+  )
+}
+
+export default App
