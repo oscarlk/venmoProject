b1ff52d3161846314840819f0a3f976ea2938447
Oscar Khowong <42625208+oscarlk@users.noreply.github.com>
2025-05-25 15:13:27 -0700

    make root page sign in page for now

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,7 @@ function App() {
     <>
        <Router>
             <Routes>
+                <Route path="/" element={<SignIn />} />
                 <Route path="/dashboard" element={<Dashboard />} />
                 <Route path="/signin" element={<SignIn />} />
             </Routes>
