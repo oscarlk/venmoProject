290bdfd9a8e7b8e7f7b9697eec688f4002784718
Joyanna Jiang <jfjiang@usc.edu>
2025-05-28 20:40:48 -0700

    nav bar component

---
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { AppBar, Toolbar, Typography, Box, IconButton, Avatar } from '@mui/material';
+import SignOutOverlay from '../SignOut/SignOutOverlay';
+
+const NavBar = () => {
+  return (
+    <AppBar position="static" color="#000" elevation={1}>
+      <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
+        
+        <Typography variant="h6" component="div" sx={{ fontWeight: 'bold' }}>
+          Stealth Project
+        </Typography>
+
+        <Box>
+          <SignOutOverlay />
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default NavBar;
--- frontend/src/pages/Dashboard/Dashboard.jsx
+++ frontend/src/pages/Dashboard/Dashboard.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
-import SignOutOverlay from '../../components/SignOut/SignOutOverlay';
+import { Container } from '@mui/material';
+import styles from './Dashboard.module.css';
+import NavBar from '../../components/NavBar/NavBar';
+import venmo from '../../assets/venmo.png';
+
+
 
 const Dashboard = () => {
   return (
-    <div>
-      dasboard 
-      <SignOutOverlay />
-    </div>
+    <>
+        <NavBar />
+        <Container maxWidth="lg" className={styles.container}>
+          
+            <h1>Your <img src={venmo} alt="Venmo" width={130} /> history, at a glance.</h1>
+
+        </Container>
+    </>
   )
 }
 
